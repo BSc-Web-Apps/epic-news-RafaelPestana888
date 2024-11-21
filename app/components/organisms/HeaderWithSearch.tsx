ad7e2930a600a80836a0484b28a2bdb0596e12f8
RafaelPestana888 <122618661+RafaelPestana888@users.noreply.github.com>
2024-11-21 15:43:41 +0000

    Update HeaderWithSearch.tsx

--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -2,6 +2,8 @@ import { useMatches } from '@remix-run/react'
 import { SearchBar } from '../molecules/SearchBar'
 import LoginOrUserDropdown from './LoginOrUserDropdown'
 import logo from '~/assets/png/logo.png';
+import { Link, useMatches } from '@remix-run/react'
+
 
 
 export default function HeaderWithSearch() {
@@ -10,11 +12,21 @@ export default function HeaderWithSearch() {
   const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
 
   return (
-    <header className="dark:bg-gray-800 bg-blue-950 py-6">
+    <header className="dark:bg-dark-primary/10 bg-primary/10 py-6">
       <nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-        <div className="flex items-center gap-4">
+	    <Link to="/">
+		<div className="flex items-center gap-4">
 		<img src={logo} alt="Epic News Logo" className="w-16" />
           <span className="text-sm text-foreground text-gray-400">Epic News</span>
+          </div>
+		</Link>
+		<div>
+          <Link
+            to="/news"
+            className="text-sm font-semibold text-muted-foreground transition hover:text-foreground"
+          >
+            News
+          </Link>
         </div>
         <div className="ml-auto hidden max-w-sm flex-1 sm:block">
           {searchBar}
