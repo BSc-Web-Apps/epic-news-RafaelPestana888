f83e6cc0998948a62dcb17e54093225707a1fc39
RafaelPestana888 <122618661+RafaelPestana888@users.noreply.github.com>
2024-11-21 15:34:41 +0000

    importing images to react

--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -1,23 +1,29 @@
 import { useMatches } from '@remix-run/react'
 import { SearchBar } from '../molecules/SearchBar'
 import LoginOrUserDropdown from './LoginOrUserDropdown'
+import logo from '~/assets/png/logo.png';
+
 
 export default function HeaderWithSearch() {
-	const matches = useMatches()
-	const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
-	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
+  const matches = useMatches()
+  const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
+  const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
 
-	return (
-		<header className="bg-primary/10 py-6 dark:bg-dark-primary/10">
-			<nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
-					{searchBar}
-				</div>
-				<div className="flex items-center gap-10">
-					<LoginOrUserDropdown />
-				</div>
-				<div className="block w-full sm:hidden">{searchBar}</div>
-			</nav>
-		</header>
-	)
+  return (
+    <header className="dark:bg-gray-800 bg-blue-950 py-6">
+      <nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+        <div className="flex items-center gap-4">
+		<img src={logo} alt="Epic News Logo" className="w-16" />
+          <span className="text-sm text-foreground text-gray-400">Epic News</span>
+        </div>
+        <div className="ml-auto hidden max-w-sm flex-1 sm:block">
+          {searchBar}
+        </div>
+        <div className="flex items-center gap-10">
+          <LoginOrUserDropdown />
+        </div>
+        <div className="block w-full sm:hidden">{searchBar}</div>
+      </nav>
+    </header>
+  )
 }
