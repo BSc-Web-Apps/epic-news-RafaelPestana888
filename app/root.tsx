c7516beb65402e816c8a704618897cc8018657f0
RafaelPestana888 <122618661+RafaelPestana888@users.noreply.github.com>
2024-11-28 16:28:55 +0000

    better

--- app/root.tsx
+++ app/root.tsx
@@ -4,51 +4,37 @@ import Document from '~/components/shared-layout/Document'
 import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
-import { loader } from './__root.server'
-import useTheme from './hooks/useTheme.tsx'
-import HeaderWithSearch from './components/organisms/HeaderWithSearch'
+import { type loader } from './__root.server'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
-import HeroCallToAction from './components/organisms/Hero/HeroCallToAction.tsx'
+import HeaderWithSearch from './components/organisms/HeaderWithSearch'
+import useTheme from './hooks/useTheme.tsx'
 
 export const links: LinksFunction = () => {
-  return rootLinkElements
+	return rootLinkElements
 }
 export { headers, meta } from './__root.client.tsx'
 export { action, loader } from './__root.server.tsx'
 
 export default function App() {
-  const data = useLoaderData<typeof loader>()
-  const nonce = useNonce()
-  const theme = useTheme()
+	const data = useLoaderData<typeof loader>()
+	const nonce = useNonce()
+	const theme = useTheme()
 
-  return (
-    <Document nonce={nonce} theme={theme}>
-      <div className="flex h-screen flex-col justify-between">
-        <HeaderWithSearch />
+	return (
+		<Document nonce={nonce} theme={theme}>
+			<div className="flex h-screen flex-col justify-between">
+				<HeaderWithSearch />
 
-        <div className="flex-1">
-          {/* <main className="grid h-full place-items-center">
-            <h1 className="text-mega">Welcome to Epic News!</h1>
-            <div className="w-full py-16">
-              <HeroCallToAction image="https://www.helpguide.org/wp-content/uploads/2020/01/Social-Media-and-Mental-Health.webp">
-                <div className="flex flex-col gap-8 px-8">
-                  <h2 className="text-h2">Welcome to Epic News</h2>
-                  <p className="text-lg">
-                    Keep up to date with the latest tech news.
-                  </p>
-                  </div>
-                </HeroCallToAction>
-            </div>
-          </main> */}
-          <Outlet />
-        </div>
+				<div className="flex-1">
+					<Outlet />
+				</div>
 
-        <div className="container flex justify-between pb-5">
-          <ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
-        </div>
+				<div className="container flex justify-between pb-5">
+					<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+				</div>
 
-        <FooterMenuRight />
-      </div>
-    </Document>
-  )
+				<FooterMenuRight />
+			</div>
+		</Document>
+	)
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,22 @@
 import { type MetaFunction } from '@remix-run/node'
+import HeroCallToAction from '#app/components/organisms/Hero/HeroCallToAction.tsx'
 
 export const meta: MetaFunction = () => [{ title: 'Epic News' }]
 
 export default function Index() {
 	return (
 		<main className="grid h-full place-items-center">
-			<h1 className="text-mega">
-				Hello from{' '}
-				<pre className="prose rounded-lg bg-primary p-6 text-primary-foreground">
-					app/routes/_index.tsx
-				</pre>
-			</h1>
+			<h1 className="text-mega">Welcome to Epic News!</h1>
+			<div className="w-full py-16">
+				<HeroCallToAction image="https://www.helpguide.org/wp-content/uploads/2020/01/Social-Media-and-Mental-Health.webp">
+					<div className="flex flex-col gap-8 px-8">
+						<h2 className="text-h2">Welcome to Epic News</h2>
+						<p className="text-lg">
+							Keep up to date with the latest tech news.
+						</p>
+					</div>
+				</HeroCallToAction>
+			</div>
 		</main>
 	)
 }
