e5de757975d77c38d849458ecd987336872c160c
RafaelPestana888 <122618661+RafaelPestana888@users.noreply.github.com>
2024-12-19 16:31:29 +0000

    Create a single article page

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -4,8 +4,8 @@ import {
   MdOutlineDesktopMac,
   MdOutlineNewspaper,
 } from 'react-icons/md'
-import siteLogo from '~/assets/png/logo.png'
-import { getArticleImgSrc } from '~/utils/misc.tsx'
+
+
 
 interface ArticleCardProps {
   title: string
@@ -18,7 +18,7 @@ export default function ArticleCard({
   category = 'General news',
   imageId,
 }: ArticleCardProps) {
-  const imageSrc = imageId ? getArticleImgSrc(imageId) : siteLogo
+
 
   const categoryIcons: { [key: string]: JSX.Element } = {
     Business: <MdOutlineBusinessCenter size={20} className="text-red-300" />,
@@ -29,12 +29,6 @@ export default function ArticleCard({
 
   return (
     <div>
-      <div>
-        <img
-          src={imageSrc}
-          alt={title}
-        />
-      </div>
       <div className="flex h-64 cursor-pointer flex-col justify-between rounded bg-red-900 p-4 transition-all duration-500 hover:scale-110">
         <h3 className="line-clamp-3 text-xl font-bold">{title}</h3>
 
